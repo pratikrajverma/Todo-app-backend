ad8212fd792ab6de3551912e201d6f5cd8063beb
pratik <erpratikrajverma@gmail.com>
2024-01-29 17:41:48 +0530

    #

--- routes/todosRouter.js
+++ routes/todosRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const {createTodo} = require("../controllers/createTodo");
 
 //define api routes
-router.post("/createTodo",()=>createTodo);
+router.post("/createTodo", createTodo);
 
 module.exports = router;
