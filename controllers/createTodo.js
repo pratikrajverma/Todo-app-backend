ee5e526b5ae954ff6d2255c016522ce419ebd4e4
pratik <erpratikrajverma@gmail.com>
2024-02-05 10:27:21 +0530

    #

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -4,39 +4,37 @@ const Todo = require('../models/Todo');   //here we are importing model from the
 //define route handler
 const createTodo = async (req, res) => {
 
-    try{
+    try {
         //extracting data form request body 
-        const{title,description,brand} = req.body;
+        const { title, description, brand } = req.body;
         //create new todo object and send to DB
-        const response = await Todo.create({title,description,brand}); //Mongoose ke create method ka response me ek JavaScript object hota hai, jisme aapko create kiye gaye document ke details milte hain. Commonly, isme document ka _id (MongoDB document identifier) aur doosre fields ki values hoti hain.
+        const response = await Todo.create({ title, description, brand }); //Mongoose ke create method ka response me ek JavaScript object hota hai, jisme aapko create kiye gaye document ke details milte hain. Commonly, isme document ka _id (MongoDB document identifier) aur doosre fields ki values hoti hain.
         //send a json response with success flag
         res.status(200).json(  // res.status(200) ka MATLAB hai ki HTTP response ka status code 200 hai, jo ki "OK" ko represent karta hai. Jab server kisi client ko 200 status code ke saath response bhejta hai, toh yeh batata hai ki client ka request server ne successfully process kiya hai aur usne client ko sahi response diya hai
             {
-                success:true, //bhejna ka maksad hai ki client ko ek clear indication mile ki unka request sahi tarike se process hua hai
-                data:response,
-                message:'entry created successfully'
+                success: true, //bhejna ka maksad hai ki client ko ek clear indication mile ki unka request sahi tarike se process hua hai
+                data: response,
+                message: 'entry created successfully'
             }
         );
- 
-   
-        
+
+
+
     }
-    catch(err)
-    {
-    
+    catch (err) {
+
         console.error(err);  //console.error se error ko highlight kara jata hai, aur console.log se bhi detailed information mil jati hai.
         console.log(err);
         res.status(500).json(  // Jab koi error catch hoti hai, toh server HTTP response mein status code 500 bhejta hai, jo Internal Server Error ko indicate karta hai
-        {
-            success:false,
-                data:"internal server error",
-                message:err.message
+            {
+                success: false,
+                error: err.message,
+                message: "internal server error"
             }
         );
 
     }
 }
 
- module.exports = {createTodo};
+module.exports = { createTodo };
 
- 
